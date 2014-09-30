f05259f6cdfe05bb87f25573389e1a04ced41e98
HarryZhu <7harryprince@gmail.com>
2014-09-30 09:16:01 +0800

    去掉冗余行

--- 整数规划Integer Programming/整数规划Integer Programming.R
+++ 整数规划Integer Programming/整数规划Integer Programming.R
@@ -9,40 +9,24 @@
 library(Rglpk)
 ##正式工作##########################
 <<<<<<< HEAD
+##价值系数Xi向量 a numeric vector representing the objective coefficients
 obj <- c(3,2,2.9)
-##最优系数Xi向量 a numeric vector representing the objective coefficients
+##费用矩阵 a numeric vector or a matrix of constraint coefficients.
 x1<- c(8,10,2)
 x2<- c(2,5,13)
 x3<- c(10,8,10)
 data<- data.frame(x1,x2,x3)
 data
-##费用矩阵 a numeric vector or a matrix of constraint coefficients.
-m=nrow(data)
- dir <- rep("<=",m)
+##右侧
+m=nrow(data)##约束条件个数 the numbers of constraints
 ##方向(大于还是小于) a character vector with the directions of the constraints. Each element must be one of "<", "<=", ">", ">=", or "==".
- rhs <- c(300,400,420)   
-##B值 the right hand side of the constraints.
- types <- rep("C",m)## 或者"I",        
+dir <- rep("<=",m)
+##右侧B值 the right hand side of the constraints.
+rhs <- c(300,400,420)   
 ##变量类型 "B" for binary, "C" for continuous or "I" for integer. By default NULL, taken as all-continuous. Recycled as needed.
-=======
-##最优系数Xi向量 a numeric vector representing the objective coefficients
-obj <- c(10, 2, 4,5)
-##费用矩阵 a numeric vector or a matrix of constraint coefficients.
-x1<- c(-1,0,1,5)
-x2<- c(3,2,4,-3)
-x3<- c(1,5,-3,2)
-x4<- c(2,3,4,2)
-data<- data.frame(x1,x2,x3,x4)
-data
-##方向(大于还是小于) a character vector with the directions of the constraints. Each element must be one of "<", "<=", ">", ">=", or "==".
- dir <- rep("<=", 4)
-##B值 the right hand side of the constraints.
- rhs <- c(4, 2, 3,6)   
-##变量类型：可以使整数型I、连续型C等 ；"B" for binary, "C" for continuous or "I" for integer. By default NULL, taken as all-continuous. Recycled as needed.
- types <- c("I", "C", "I","I")        
-##整数规划函数 Rgplk_solve_LP(Object,Matrix,Direction,RightHandSide,MaximunOrMinimun)
->>>>>>> FETCH_HEAD
- Rglpk_solve_LP(obj, data, dir, rhs, types, max = TRUE)
+types <- rep("C",m)## 或者"I",        
+##准备求解原问题 primary problem=======
+Rglpk_solve_LP(obj, data, dir, rhs, types, max = TRUE)
 ##运行结果running result
 
 #$optimum
